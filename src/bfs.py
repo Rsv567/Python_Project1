864ec784f048175d4e3fa819993c446577e21f7a
Svyatoslav Ryazanov <144775578+Rsv567@users.noreply.github.com>
2023-11-22 21:11:53 +0300

    Update bfs.py

--- src/bfs.py
+++ src/bfs.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 import pygame
 
 from src.config import TILE
@@ -7,6 +8,7 @@ from src.config import rows
 from src.generator_of_labirint import grid_cells
 from src.generator_of_labirint import screen
 from src.generator_of_labirint import clock
+
 
 class Bfs:
     def create_graph(self):
