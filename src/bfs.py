3dcfc100f55397ea000f62de5eb4839bed8f3702
Svyatoslav Ryazanov <144775578+Rsv567@users.noreply.github.com>
2023-11-22 18:54:06 +0300

    Update bfs.py

--- src/bfs.py
+++ src/bfs.py
@@ -1,106 +1,72 @@
-from collections import deque
-from generator_of_labirint import *
+import pygame
+from src.dijkstra import Dijkstra
+from src.dijkstra import screen1
+from src.dijkstra import clock
+from src.generator_of_labirint import generate_labirint
+from src.bfs import Bfs
+from src.button import ImageButton
+from src.config import WIDTH
+from src.config import HEIGHT
 
-class Bfs:
-    def create_graph(self):
-        graph = {}
-        for i in range(cols * rows):
-            x_cord = grid_cells[i].x
-            y_cord = grid_cells[i].y
-            if not grid_cells[i].walls["top"]:
-                graph[(x_cord, y_cord)] = graph.get((x_cord, y_cord), []) + [
-                    (x_cord, y_cord - 1)
-                ]
-            if not grid_cells[i].walls["bottom"]:
-                graph[(x_cord, y_cord)] = graph.get((x_cord, y_cord), []) + [
-                    (x_cord, y_cord + 1)
-                ]
-            if not grid_cells[i].walls["left"]:
-                graph[(x_cord, y_cord)] = graph.get((x_cord, y_cord), []) + [
-                    (x_cord - 1, y_cord)
-                ]
-            if not grid_cells[i].walls["right"]:
-                graph[(x_cord, y_cord)] = graph.get((x_cord, y_cord), []) + [
-                    (x_cord + 1, y_cord)
-                ]
-        return graph
+pygame.init()
 
+pygame.display.set_caption("Labirint")
+generator1 = ImageButton(
+    WIDTH / 2 - (150 / 2),
+    70,
+    150,
+    74,
+    "Лабиринт",
+    "images/red.png",
+    "images/green.com.png",
+)
 
-    def get_rect(self,x, y):
-        return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
-
-
-    def find_way(self):
-        start = (0, 0)
-        goal = start
-        queue = deque([start])
-        visited = {start: None}
-
-        running = True
-
-        while running:
-            [pygame.draw.rect(screen, (19, 242, 17), self.get_rect(x, y)) for x, y in visited]
-            [pygame.draw.rect(screen, (19, 242, 17), self.get_rect(x, y)) for x, y in queue]
-            graph = self.create_graph()
-            # BFS logic
-
-            mouse_pos = self.get_click_mouse_pos(screen)
-            if mouse_pos:
-                queue, visited = self.bfs(start, mouse_pos, graph)
-                goal = mouse_pos
-
-            # draw path
-            path_head, path_segment = goal, goal
-            while path_segment and path_segment in visited:
-                pygame.draw.rect(
-                    screen,
-                    pygame.Color("white"),
-                    self.get_rect(*path_segment),
-                    TILE,
-                    border_radius=TILE // 3,
-                )
-                path_segment = visited[path_segment]
-                pygame.draw.rect(
-                    screen, pygame.Color("blue"), self.get_rect(*start), border_radius=TILE // 3
-                )
-                pygame.draw.rect(
-                    screen,
-                    pygame.Color("magenta"),
-                    self.get_rect(*path_head),
-                    border_radius=TILE // 3,
-                )
-
-            pygame.display.flip()
-            clock.tick(10)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                    pygame.quit()
+dijkstra1 = ImageButton(
+    WIDTH / 2 - (150 / 2),
+    200,
+    150,
+    74,
+    "Дейкстра",
+    "images/red.png",
+    "images/green.com.png",
+)
+deikstra = Dijkstra()
+generate = generate_labirint()
+bfs2 = Bfs()
 
 
-    def get_click_mouse_pos(self,screen):
-        x, y = pygame.mouse.get_pos()
-        cell_x, cell_y = x // TILE, y // TILE
+def MainMenu():
+    running = True
+    while running:
 
-        click = pygame.mouse.get_pressed()
-        if click[0]:
-            pygame.draw.rect(screen, pygame.Color("red"), self.get_rect(cell_x, cell_y))
-        return (cell_x, cell_y) if click[0] else False
+        screen1.fill((243, 215, 18))
+
+        screen1.blit(
+            pygame.transform.scale(
+                pygame.image.load("images/icon.png"), (WIDTH, HEIGHT)
+            ),
+            (0, 0),
+        )
+
+        generator1.check_hover(pygame.mouse.get_pos())
+        generator1.draw(screen1)
+
+        dijkstra1.check_hover(pygame.mouse.get_pos())
+        dijkstra1.draw(screen1)
+
+        pygame.display.flip()
+        clock.tick(100)
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT and event.button == generator1:
+                generate.generate_labirint()
+                bfs2.find_way()
+            if event.type == pygame.USEREVENT and event.button == dijkstra1:
+                deikstra.dijkstra()
+            generator1.handle_event(event)
+            dijkstra1.handle_event(event)
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 
 
-    def bfs(self,start, goal, graph):
-        queue = deque([start])
-        visited = {start: None}
-
-        while queue:
-            cur_node = queue.popleft()
-            if cur_node == goal:
-                break
-
-            next_nodes = graph[cur_node]
-            for next_node in next_nodes:
-                if next_node not in visited:
-                    queue.append(next_node)
-                    visited[next_node] = cur_node
-        return queue, visited
+MainMenu()
